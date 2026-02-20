6ffc4d66ca54c552973bf944b2e1393bca4fbdad
simpa31415 <simon.astrand@gmail.com>
2026-02-20 15:30:53 +0100

    justering

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 from flask import Flask, render_template
-import os
 
+# Skapa Flask-appen
 app = Flask(__name__)
 
+# Route för startsidan
 @app.route("/")
 def home():
     return render_template("index.html")
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
